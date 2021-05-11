bab8ac7166d9ba10c647d796ec71f62fabec3f22
Prerak Lodha <57831679+prerakl123@users.noreply.github.com>
2021-05-11 20:10:55 +0530

    Add files via upload

--- converters/deletethis.py
+++ converters/deletethis.py
@@ -1,6 +1,48 @@
-def abcd():
-    import os
+# def abcd():
+#     import os
+#
+#     print(os.getcwd())
+#     with open(f'{os.getcwd()}\\converters\\currencies\\USD.json') as file:
+#         print(file.read())
 
-    print(os.getcwd())
-    with open(f'{os.getcwd()}\\converters\\currencies\\USD.json') as file:
-        print(file.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+name = input("Window name: ")
+os.system("""ffmpeg -rtbufsize 1500M -f dshow -i audio="Stereo Mix (Realtek High Definition Audio)" -framerate 60 -pix_fmt yuv420p -profile:v baseline -y output2.mp4""")
